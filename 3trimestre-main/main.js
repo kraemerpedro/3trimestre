095832781f8c32452dd5558f1a1f380f3903bcf8
kraemerpedro <125660227+kraemerpedro@users.noreply.github.com>
2023-11-01 13:49:13 -0300

    Add files via upload

---
+++ 3trimestre-main/main.js
@@ -0,0 +1,18 @@
+
+function tocaSomPom(){
+ document.querySelector('#som_tecla_pom').play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador= 0; 
+while(contador >listadeteclas.length){
+
+listadeteclas[0].onclik=tocasompom
+contador=contador + 1;
+
+console.log(contador);
+
+
+
+
+}
